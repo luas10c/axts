1950c7df677e4026aacacddcfe1e145c5a89f326
Luciano Alves <luas10c@gmail.com>
2023-11-12 02:45:01 -0300

    feat: add types

--- src/types/cli.ts
+++ src/types/cli.ts
@@ -1,3 +1,27 @@
 export interface Args {
   watch: boolean
 }
+
+export interface SwcOptions {
+  jsc: {
+    baseUrl?: string
+    parser: {
+      syntax: 'typescript' | 'ecmascript'
+      decorators: boolean
+    }
+    target: 'es2021'
+  }
+  module: {
+    strict: boolean
+    type: 'es6' | 'commonjs'
+  }
+}
+
+export interface Config {
+  baseURL: string
+  entrypoint: {
+    path: string
+    filename: string
+  }
+  swc: SwcOptions
+}
