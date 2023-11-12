c9e37f74fb409cee0f62f4087200cc6435807230
Luciano Alves <luas10c@gmail.com>
2023-11-12 02:47:05 -0300

    feat: add path from file

--- src/builders/swc.ts
+++ src/builders/swc.ts
@@ -4,50 +4,21 @@ import { writeFile } from 'node:fs/promises'
 
 import { store } from '#/utils/store.js'
 
-async function build(pathname: string) {
-  const { dir, name, ext } = path.parse(pathname)
+async function build(item: string) {
+  const { dir, base, name } = path.parse(
+    item
+      .split(store.config.entrypoint.path)
+      .slice(-1)
+      .join()
+      .replace(/^(?:\/\/?)/, '')
+  )
 
   const { code } = await transformFile(
-    path.join(store.config.baseURL, dir, `${name}${ext}`),
-    {
-      jsc: {
-        baseUrl: process.cwd(),
-        parser: {
-          syntax: 'typescript',
-          decorators: true,
-          tsx: false
-        },
-        target: 'es2021',
-        paths: {
-          '#/*': ['./src/*']
-        }
-      },
-      sourceMaps: false,
-      module: {
-        strict: true,
-        type: 'es6'
-      }
-    }
+    path.join(store.config.baseURL, store.config.entrypoint.path, dir, base),
+    store.config.swc
   )
 
-  console.log(
-    `Compiled file: ${path.join(
-      store.config.baseURL,
-      'dist',
-      dir.split('/').splice(1).join(),
-      `${name}${ext}`
-    )}`
-  )
-
-  await writeFile(
-    path.join(
-      store.config.baseURL,
-      'dist',
-      dir.split('/').splice(1).join(),
-      `${name}.js`
-    ),
-    code
-  )
+  await writeFile(path.join(store.config.baseURL, 'dist', dir, `${name}.js`), code)
 }
 
 export const swc = {
