180913e6285be1cab21fcf448b2a300a7b7674e3
Luciano Alves <luas10c@gmail.com>
2023-12-11 03:49:55 -0300

    feat: resolves tsconfig.json paths

--- src/builders/swc.ts
+++ src/builders/swc.ts
@@ -1,28 +1,37 @@
 import path from 'node:path'
 import { transformFile } from '@swc/core'
-import { writeFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 
-import { store } from '#/utils/store.js'
+import { store } from '#/store.js'
 
-async function build(item: string) {
-  const { dir, base, name } = path.parse(
-    item
-      .split(store.config.entrypoint.path)
-      .slice(-1)
-      .join()
-      .replace(/^(?:\/\/?)/, '')
-  )
-
+async function build(filename: string) {
   const { code } = await transformFile(
-    path.join(store.config.baseURL, store.config.entrypoint.path, dir, base),
-    store.config.swc
+    path.join(store.baseURL, store.entrypoint.at(0) as string, filename),
+    store.swc
   )
+  await writeFile(path.join(store.baseURL, 'dist', filename.replace('.ts', '.js')), code)
+}
 
-  await writeFile(path.join(store.config.baseURL, 'dist', dir, `${name}.js`), code)
+async function config() {
+  try {
+    const { compilerOptions } = await readFile(
+      path.join(store.baseURL, 'tsconfig.json'),
+      'utf-8'
+    ).then((data) => JSON.parse(data))
+
+    if (compilerOptions?.paths) {
+      store.swc.jsc!.paths = compilerOptions.paths
+    }
+  } catch {}
+
+  if (store.cli.commonjs) {
+    store.swc.module!.type = 'commonjs'
+  }
 }
 
 export const swc = {
-  build
+  build,
+  config
 }
 
 export default swc
